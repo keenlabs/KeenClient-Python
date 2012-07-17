99983810aee336f43fce56ac1361a4bdb9f59175
Daniel Kador <dkador@gmail.com>
2012-07-17 00:30:57 -0400

    remove nose as requirement for running client, add it as requirement for tests

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 setup(name="keen-client",
-      version="0.1.1",
+      version="0.1.2",
       description="Python Client for Keen.io",
       author="Keen Labs",
       author_email="team@keen.io",
       url="https://github.com/keenlabs/KeenClient-Python",
       packages=["keen"],
-      install_requires=["requests", "nose"]
+      install_requires=["requests"],
+      tests_require=["nose"]
 )
