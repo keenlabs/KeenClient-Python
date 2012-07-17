26627e882650718ed811842337dda28b6011ae4e
Daniel Kador <dkador@gmail.com>
2012-07-17 00:24:29 -0400

    require nose, bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name="keen-client",
-      version="0.1.0",
+      version="0.1.1",
       description="Python Client for Keen.io",
       author="Keen Labs",
       author_email="team@keen.io",
       url="https://github.com/keenlabs/KeenClient-Python",
       packages=["keen"],
-      install_requires=["requests"]
+      install_requires=["requests", "nose"]
 )
