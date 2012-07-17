f30ee788485962574b68f3a19ea60f414d9a8f15
Daniel Kador <dkador@gmail.com>
2012-07-16 23:18:08 -0400

    change version for client to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='keen-client',
-      version='0.1',
+      version='0.1.0',
       description='Python Client for Keen.io',
       author='Keen Labs',
       author_email='team@keen.io',
