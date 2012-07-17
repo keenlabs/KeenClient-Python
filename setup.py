dd98376188b4ad6a0aaa6551a2801b940588f9d9
Daniel Kador <dkador@gmail.com>
2012-07-16 21:44:25 -0400

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='KeenClient',
+      version='0.1',
+      description='Python Client for Keen.io',
+      author='Keen Labs',
+      author_email='team@keen.io',
+      url='https://github.com/keenlabs/KeenClient-Python',
+      packages=['keen'],
+     )
