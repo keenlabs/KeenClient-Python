096a1d94c2f54246d51954b59fc5c3fdb28154b2
Daniel Kador <dkador@gmail.com>
2012-07-16 22:55:15 -0400

    add persistence strategy enum

--- keen/__init__.py
+++ keen/__init__.py
@@ -1,1 +1,12 @@
 __author__ = 'dkador'
+
+class PersistenceStrategy:
+    """
+    An enum that defines the persistence strategy used by the KeenClient.
+    Currently supported: DIRECT, which means any time add_event is called the
+    client will call out directly to Keen, or REDIS, which means add_event
+    will simply add the event to a defined Redis instance which can be
+    cleared later.
+    """
+    DIRECT = 0,
+    REDIS = 1
