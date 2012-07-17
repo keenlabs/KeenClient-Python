70a2f716dd4a8efc7cda65329645c666c4bc94fd
Daniel Kador <dkador@gmail.com>
2012-07-17 00:23:35 -0400

    replace single quotes with double quotes in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='keen-client',
-      version='0.1.0',
-      description='Python Client for Keen.io',
-      author='Keen Labs',
-      author_email='team@keen.io',
-      url='https://github.com/keenlabs/KeenClient-Python',
-      packages=['keen'],
-      install_requires=['requests']
+setup(name="keen-client",
+      version="0.1.0",
+      description="Python Client for Keen.io",
+      author="Keen Labs",
+      author_email="team@keen.io",
+      url="https://github.com/keenlabs/KeenClient-Python",
+      packages=["keen"],
+      install_requires=["requests"]
      )
