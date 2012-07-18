53bab860a521944d0b5719525c80c656c760041f
Daniel Kador <dkador@gmail.com>
2012-07-17 23:36:45 -0400

    add docstrings to client

--- keen/client.py
+++ keen/client.py
@@ -12,6 +12,14 @@ class Event(object):
 
     def __init__(self, project_id, collection_name, event_body,
                  timestamp=None):
+        """ Initializes a new Event.
+
+        :param project_id: the Keen project ID to insert the event to
+        :param collection_name: the Keen collection name to insert the event to
+        :param event_body: a dict that contains the body of the event to insert
+        :param timestamp: optional, specify a datetime to override the
+        timestamp associated with the event in Keen
+        """
         super(Event, self).__init__()
         self.project_id = project_id
         self.collection_name = collection_name
@@ -19,6 +27,10 @@ class Event(object):
         self.timestamp = timestamp
 
     def to_json(self):
+        """ Serializes the event to JSON.
+
+        :returns: a string
+        """
         event_as_dict = {
             "body": self.event_body
         }
@@ -28,7 +40,24 @@ class Event(object):
 
 
 class KeenClient(object):
+    """
+    The Keen Client is the main object to use to interface with Keen. It
+    requires a project ID and an auth token. Optionally,
+    you can also specify a persistence strategy to elect how events are
+    handled when they're added. The default strategy is to send the event
+    directly to Keen, in-line. This may not always be the best idea, though,
+    so we support other strategies (such as persisting to a local Redis queue
+    for later processing).
+    """
+
     def __init__(self, project_id, auth_token, persistence_strategy=None):
+        """ Initializes a KeenClient object.
+
+        :param project_id: the Keen project ID
+        :param auth_token: the Keen auth token
+        :param persistence_strategy: optional, the strategy to use to persist
+        the event
+        """
         super(KeenClient, self).__init__()
 
         # do some validation
@@ -50,6 +79,17 @@ class KeenClient(object):
         self.persistence_strategy = persistence_strategy
 
     def add_event(self, collection_name, event_body, timestamp=None):
+        """ Adds an event.
+
+        Depending on the persistence strategy of the client,
+        this will either result in the event being uploaded to Keen
+        immediately or will result in saving the event to some local cache.
+
+        :param collection_name: the name of the collection to insert the
+        event to
+        :param event_body: dict, the body of the event to insert the event to
+        :param timestamp: datetime, optional, the timestamp of the event
+        """
         event = Event(self.project_id, collection_name, event_body,
                       timestamp=timestamp)
         self.persistence_strategy.persist(event)
