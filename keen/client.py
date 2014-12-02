2aeca0d4c3131fd7f267c345337d8d8d60b5745c
Stephanie Stroud <stroud109@gmail.com>
2014-12-01 20:36:10 -0800

    Removed `max_age` from the extraction method.

--- keen/client.py
+++ keen/client.py
@@ -399,7 +399,7 @@ class KeenClient(object):
         return self.api.query("select_unique", params)
 
     def extraction(self, event_collection, timeframe=None, timezone=None, filters=None, latest=None,
-                   email=None, property_names=None, max_age=None):
+                   email=None, property_names=None):
         """ Performs a data extraction
 
         Returns either a JSON object of events or a response
@@ -415,12 +415,10 @@ class KeenClient(object):
         :param latest: int, the number of most recent records you'd like to return
         :param email: string, optional string containing an email address to email results to
         :param property_names: string or list of strings, used to limit the properties returned
-        :param max_age: an integer, greater than 30 seconds, the maximum ‘staleness’ you’re
-        willing to trade for increased query performance, in seconds
+
         """
         params = self.get_params(event_collection=event_collection, timeframe=timeframe, timezone=timezone,
-                                 filters=filters, latest=latest, email=email, property_names=property_names,
-                                 max_age=max_age)
+                                 filters=filters, latest=latest, email=email, property_names=property_names)
         return self.api.query("extraction", params)
 
     def funnel(self, steps, timeframe=None, timezone=None, max_age=None):
