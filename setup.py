fbf3b7a52dd1d02a79b9156d51e511b322edcdd7
Stephanie Stroud <stroud109@gmail.com>
2015-01-31 20:59:34 -0800

    Updated setup.py so `python setup.py test` command works. Tested in 2.6 and 2.7.

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup, find_packages
+import sys
 
-setup(name="keen",
-      version="0.3.7",
-      description="Python Client for Keen IO",
-      author="Keen IO",
-      author_email="team@keen.io",
-      url="https://github.com/keenlabs/KeenClient-Python",
-      packages=["keen"],
-      install_requires=["requests", "pycrypto", "Padding"],
-      tests_require=["nose"]
-      )
+try:
+    import multiprocessing  # NOQA
+except ImportError:
+    pass
+
+tests_require = ['nose']
+
+if sys.version_info < (2, 7):
+    tests_require.append('unittest2')
+
+setup_requires = []
+if 'nosetests' in sys.argv[1:]:
+    setup_requires.append('nose')
+
+setup(
+    name="keen",
+    version="0.3.7",
+    description="Python Client for Keen IO",
+    author="Keen IO",
+    author_email="team@keen.io",
+    url="https://github.com/keenlabs/KeenClient-Python",
+    packages=["keen"],
+    install_requires=["requests", "pycrypto", "Padding"],
+    tests_require=tests_require,
+    test_suite='nose.collector',
+)
