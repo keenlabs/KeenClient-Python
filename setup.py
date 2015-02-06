9daec2300c2cc8a4b3aa279ff8342c54e1a78b70
Stephanie Stroud <stroud109@gmail.com>
2015-02-06 14:37:03 -0800

    Bumped up the version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if 'nosetests' in sys.argv[1:]:
 
 setup(
     name="keen",
-    version="0.3.7",
+    version="0.3.8",
     description="Python Client for Keen IO",
     author="Keen IO",
     author_email="team@keen.io",
