a6b7390ae7f435bd330d856b4ac6a81a999e5845
Stephanie Stroud <stroud109@gmail.com>
2015-03-22 18:55:18 -0700

    Removed test requirements from `requirements.txt`

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt', session=pip.download.PipSession())
-tests_require = ['nose']
+tests_require = ['nose', 'mock']
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
