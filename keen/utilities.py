14a25c0de7585d928b5504267441de3fbc9ecffc
Joe Wegner <joe@wegnerdesign.com>
2017-02-22 14:43:11 -0600

    spacing

--- keen/utilities.py
+++ keen/utilities.py
@@ -3,26 +3,26 @@ import os
 VERSION = None
 
 def version():
-  """
-  Retrieves the current version of the SDK
-  """
+    """
+    Retrieves the current version of the SDK
+    """
 
-  global VERSION
-  if VERSION is None:
+    global VERSION
+    if VERSION is None:
     version_file = open(os.path.join('.', 'VERSION'))
     VERSION = version_file.read().strip()
 
-  return VERSION
+    return VERSION
 
 def headers(api_key):
-  """
-  Helper function to easily get the correct headers for an endpoint
+    """
+    Helper function to easily get the correct headers for an endpoint
 
-  :params api_key: The appropriate API key for the request being made
-  """
+    :params api_key: The appropriate API key for the request being made
+    """
 
-  return {
+    return {
       "Content-Type": "application/json",
       "Authorization": api_key,
       "Keen-Sdk": "python-{0}".format(version())
-  }
+    }
