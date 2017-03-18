8fbb3c38116e26863326ff80e66167d82f335115
Justin <Justin.Mason@outlook.com>
2017-03-18 03:15:22 -0500

    Example of what would need to happen to support updating partial query definitions, like only { 'query_name': 'new_name' }.

--- keen/saved_queries.py
+++ keen/saved_queries.py
@@ -65,14 +65,65 @@ class SavedQueriesInterface:
         return response
 
     @requires_key(KeenKeys.MASTER)
-    def update(self, query_name, saved_query):
+    def update(self, query_name, saved_query_full_definition):
         """
         Updates the saved query via a PUT request to Keen IO Saved Query
-        endpoint.
+        endpoint. The entire query definition must be provided--anything
+        excluded will be considered an explicit removal of that property.
         Master key must be set.
         """
 
-        return self.create(query_name, saved_query)
+        return self.create(query_name, saved_query_full_definition)
+
+    @requires_key(KeenKeys.MASTER)
+    def update_partial(self, query_name, saved_query_attributes):
+        """
+        Given a dict of attributes to be updated, update only those attributes
+        in the Saved Query at the resource given by 'query_name'. This will
+        perform two HTTP requests--one to fetch the query definition, and one
+        to set the new attributes. This method will intend to preserve any
+        other properties on the query.
+
+        Master key must be set.
+        """
+
+        query_name_attr_name = "query_name"
+        refresh_rate_attr_name = "refresh_rate"
+        query_attr_name = "query"
+        metadata_attr_name = "metadata"
+
+        old_saved_query = self.get(query_name)
+
+        # Create a new query def to send back. We cannot send values for attributes like 'urls',
+        # 'last_modified_date', 'run_information', etc.
+        new_saved_query = {
+            query_name_attr_name: old_saved_query[query_name_attr_name], # expected
+            refresh_rate_attr_name: old_saved_query[refresh_rate_attr_name], # expected
+            query_attr_name: {}
+        }
+
+        # If metadata was set, preserve it. The Explorer UI currently stores information here.
+        old_metadata = (old_saved_query[metadata_attr_name]
+                       if hasattr(old_saved_query, metadata_attr_name)
+                       else None)
+
+        if old_metadata:
+            new_saved_query[metadata_attr_name] = old_metadata
+
+        # Preserve any non-empty properties of the existing query. We get back values like None
+        # for 'group_by', 'interval' or 'timezone', but those aren't accepted values when updating.
+        old_query = old_saved_query[query_attr_name] # expected
+
+        # Using dict.items() in both Python 2.x and Python 3.x. iteritems() might be better in 2.7.
+        # Shallow copy since we want the entire object heirarchy to start with.
+        for (key, value) in old_query.items():
+            if value:
+                new_saved_query[query_attr_name][key] = value
+
+        # Now, recursively overwrite any attributes passed in.
+        _deep_update(new_saved_query, saved_query_attributes)
+
+        return self.create(query_name, new_saved_query)
 
     @requires_key(KeenKeys.MASTER)
     def delete(self, query_name):
@@ -86,8 +137,30 @@ class SavedQueriesInterface:
 
         return True
 
+    def _deep_update(mapping, updates):
+        # NOTE : Careful with items()/iteritems()/viewitems() on Python 2 vs 3.
+        for (key, value) in updates.items():
+            # NOTE : should really check collections/collections.abc Mapping instead of dict.
+            if isinstance(mapping, dict):
+                if isinstance(value, dict):
+                    next_level_value = _deep_update(mapping.get(key, {}), value)
+                    mapping[key] = next_level_value
+                else:
+                    mapping[key] = value
+            else:
+                # Turn the original key into a mapping if it wasn't already
+                mapping = { key: value }
+
+        return mapping
+
     def _get_json(self, http_method, url, key, *args, **kwargs):
-        response = self.api.fulfill(http_method, url, headers=utilities.headers(key), *args, **kwargs)
+        response = self.api.fulfill(
+            http_method,
+            url,
+            headers=utilities.headers(key),
+            *args,
+            **kwargs)
+
         self.api._error_handling(response)
 
         try:
